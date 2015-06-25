1210c1cf02dc4785acf9b1c4cd949f29f7e82d03
Crzyrndm <joshcrawfy@gmail.com>
2015-06-26 11:43:13 +1200

    proof of concept

---
+++ MultiMonitorScreenSpace/CamTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace MapViewTest
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class CamTest : MonoBehaviour
+    {
+        Callback OnEnter, OnLeave;
+        GameObject CameraObject;
+        Camera PCam, SSCam, TestCam;
+        //public void Start()
+        //{
+        //    OnEnter = MapView.OnEnterMapView;
+        //    MapView.OnEnterMapView = onMapViewEnter;
+        //    OnLeave = MapView.OnExitMapView;
+        //    MapView.OnExitMapView = onMapViewLeave;
+
+        //    CameraObject = new GameObject("MiniMapView");
+        //    Rect viewRect = new Rect(Screen.width / 2, Screen.height / 2, Screen.width / 2, Screen.height / 2);
+        //    TestCam = CameraObject.AddComponent<Camera>();
+        //    TestCam.CopyFrom(FlightCamera.fetch.cameras[1]);
+        //    TestCam.pixelRect = viewRect;
+        //    TestCam.depth = 0;
+        //    TestCam.farClipPlane = 100000;
+
+        //    PCam = CameraObject.AddComponent<Camera>();
+        //    PCam.CopyFrom(PlanetariumCamera.Camera);
+        //    PCam.pixelRect = viewRect;
+        //    PCam.depth = 1;
+
+        //    SSCam = CameraObject.AddComponent<Camera>();
+        //    SSCam.CopyFrom(ScaledCamera.Instance.camera);
+        //    SSCam.pixelRect = viewRect;
+        //    SSCam.depth = 2;
+
+        //    SSCam.enabled = PCam.enabled = true;
+        //}
+
+        //public void LateUpdate()
+        //{
+        //    CameraObject.transform.position = FlightGlobals.ActiveVessel.transform.position;
+        //    CameraObject.transform.LookAt(FlightGlobals.ActiveVessel.mainBody.transform.position);
+        //    CameraObject.transform.Translate(0, 0, -FlightCamera.fetch.Distance * 100);
+        //}
+
+        //public void onMapViewEnter()
+        //{
+        //    OnEnter.Invoke();
+        //    Debug.Log("entered map view");
+        //}
+
+        //public void onMapViewLeave()
+        //{
+        //    OnLeave.Invoke();
+        //    Debug.Log("left map view");
+        //}
+    }
+}
---
+++ MultiMonitorScreenSpace/ScreenResizeBase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+namespace MapViewTest
+{
+    [KSPAddon(KSPAddon.Startup.EveryScene, false)]
+    public class ScreenResizeBase : MonoBehaviour
+    {
+        public static float requestedWidth;
+        public static HashSet<string> CamsResized;
+        Camera blackoutCam;
+        GameObject cameraObject;
+        Rect mainScreenArea;
+
+        public void Start()
+        {
+            if (HighLogic.LoadedSceneIsEditor)
+                return; // editor gets funky. Huge fisheye effect and part centered on screen not camera
+            if (CamsResized == null)
+                CamsResized = new HashSet<string>();
+            requestedWidth = 1920;
+            mainScreenArea = new Rect(0, 0, requestedWidth, Screen.height);
+            foreach (Camera c in Camera.allCameras)
+            {
+                if (CamsResized.Contains(c.name))
+                    continue;
+                Rect r = c.pixelRect;
+                r.x = r.x * requestedWidth / Screen.width;
+                r.width = r.width * requestedWidth / Screen.width;
+                c.pixelRect = r;
+                CamsResized.Add(c.name);
+            }
+
+            cameraObject = new GameObject("ExtraScreenBackground");
+            blackoutCam = cameraObject.AddComponent<Camera>();
+            blackoutCam.depth = 5;
+            blackoutCam.pixelRect = new Rect(requestedWidth, 0, Screen.width - requestedWidth, Screen.height);
+            blackoutCam.backgroundColor = Color.black;
+            blackoutCam.clearFlags = CameraClearFlags.SolidColor;
+            blackoutCam.cullingMask = 0;
+        }
+
+        public void Update()
+        {
+            if (!mainScreenArea.Contains(Input.mousePosition))
+            {
+                InputLockManager.SetControlLock(ControlTypes.CAMERACONTROLS, "ScreenExtenderLock");
+            }
+            else
+            {
+                InputLockManager.RemoveControlLock("ScreenExtenderLock");
+            }
+        }
+
+        public void LateUpdate()
+        {
+        }
+
+        public void OnDestroy()
+        {
+            if (cameraObject != null)
+                Destroy(cameraObject);
+        }
+    }
+}
